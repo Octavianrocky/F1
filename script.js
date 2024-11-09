628a1e347a4838da97c8116f7ee6c16dfeb62154
Octavianrocky <101217797+Octavianrocky@users.noreply.github.com>
2024-11-09 13:13:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,11 +3,15 @@ function calcolaParametri() {
     let tempGomme = parseFloat(document.getElementById("tempGomme").value);
     let tempMotore = parseFloat(document.getElementById("tempMotore").value);
     let caricoAero = parseFloat(document.getElementById("caricoAero").value);
+    let tempAria = parseFloat(document.getElementById("tempAria").value);
+    let tempAsfalto = parseFloat(document.getElementById("tempAsfalto").value);
 
     // Aggiorna i risultati nel DOM
     document.getElementById("tempGommeResult").innerText = tempGomme.toFixed(2);
     document.getElementById("tempMotoreResult").innerText = tempMotore.toFixed(2);
     document.getElementById("caricoAeroResult").innerText = caricoAero.toFixed(2);
+    document.getElementById("tempAriaResult").innerText = tempAria.toFixed(2);
+    document.getElementById("tempAsfaltoResult").innerText = tempAsfalto.toFixed(2);
 
     // Grafico della temperatura delle gomme
     let tempGommeWidth = (tempGomme / 150) * 100;
@@ -22,35 +26,4 @@ function calcolaParametri() {
     // Grafico del carico aerodinamico
     let caricoAeroWidth = (caricoAero / 10) * 100;
     if (caricoAeroWidth > 100) caricoAeroWidth = 100;
-    document.getElementById("caricoAeroBar").style.width = caricoAeroWidth + "%";
-
-    // Feedback
-    let feedbackText = "";
-
-    // Feedback sulle gomme
-    if (tempGomme < 90) {
-        feedbackText += "<p class='alert'>Le gomme sono troppo fredde, attenzione!</p>";
-    } else if (tempGomme > 110) {
-        feedbackText += "<p class='alert'>Le gomme sono troppo calde, attenzione!</p>";
-    } else {
-        feedbackText += "<p>Le gomme sono nella temperatura ottimale.</p>";
-    }
-
-    // Feedback sul motore
-    if (tempMotore > 120) {
-        feedbackText += "<p class='alert'>ATTENZIONE: Temperatura del motore troppo alta!</p>";
-    } else {
-        feedbackText += "<p>Temperatura del motore ok.</p>";
-    }
-
-    // Feedback sul carico aerodinamico
-    if (caricoAero < 2.5 || caricoAero > 5.0) {
-        feedbackText += "<p class='alert'>Il carico aerodinamico non è ottimale.</p>";
-    } else {
-        feedbackText += "<p>Carico aerodinamico ottimale.</p>";
-    }
-
-    // Mostra la sezione dei risultati
-    document.getElementById("feedback").innerHTML = feedbackText;
-    document.getElementById("resultSection").style.display = "block";
-}
+    document.getElementById("caricoAeroBar
